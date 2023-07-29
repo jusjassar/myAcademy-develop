9210701472088c6b731f083b0996143e13ab105c
Jus Jassar <jusjassar@gmail.com>
2023-07-29 16:53:50 -0400

    fixed path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { AppClassDirective } from './app-class-directive.directive';
-import { myBorderDirective } from './border-class.directive';
+import { myBorderDirective } from './customDirectives/border-class.directive';
 
 import { DemoComponent } from './demo/demo.component';
 import { HostBindingDirective } from './customDirectives/host-binding.directive';
