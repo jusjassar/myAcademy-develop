a820bfdefd60c426a90951f1b947fa253af35c7c
Jus Jassar <jusjassar@gmail.com>
2023-08-01 22:54:26 -0400

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myAcademy-develop';
+  videos= [
+    {title: 'My video 1', share: 415, likes: 257, dislikes: 12, thumbnail: '../assets/image1.jpg'},
+    {title: 'My video 2', share: 215, likes: 325, dislikes: 12, thumbnail: '../assets/image2.jpg'},
+    {title: 'My video 3', share: 513, likes: 105, dislikes: 12, thumbnail: '../assets/image3.jpg'}
+  ]
 }
