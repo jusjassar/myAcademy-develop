d3f6d7123a5230dbdcc12fb0e9088835ef60f3be
Jus Jassar <jusjassar@gmail.com>
2023-08-03 10:48:09 -0400

    betteWay

--- src/app/custom-app-style.directive.ts
+++ src/app/custom-app-style.directive.ts
@@ -3,17 +3,23 @@ import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 @Directive({
   selector: '[styleAttr]'
 })
-export class CustomAppStyleDirective implements OnInit {
+export class CustomAppStyleDirective  {
 
   constructor(private element:ElementRef,private renderer:Renderer2) { }
 
-  @Input() styleAttr:Object={}
-  
-  ngOnInit(){
-      let styleVals = Object.entries(this.styleAttr)
-      for( let [prop,val] of styleVals){
-        console.log(styleVals)
-        this.renderer.setStyle(this.element.nativeElement,prop,val)
+  @Input() set styleAttr(val:Object){
+      console.log(val)
+      let entries = Object.entries(val)
+      for( let [stylProp,val] of entries){
+        this.renderer.setStyle(this.element.nativeElement,stylProp,val)
       }
   }
+  
+  // ngOnInit(){
+  //     let styleVals = Object.entries(this.styleAttr)
+  //     for( let [prop,val] of styleVals){
+  //       console.log(styleVals)
+  //       this.renderer.setStyle(this.element.nativeElement,prop,val)
+  //     }
+  // }
 }
