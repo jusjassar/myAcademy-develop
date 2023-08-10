f25c8c90e5b122d133ca6bd50251437dd9e9ae00
jus@laptop <millionaireme@Umelaptop>
2023-08-10 14:18:12 -0400

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CoursesService } from './Services/courses.service';
-import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './ErrorPage/error.component';
 
-const myRoutes:Routes=[
-  {path:'',component:HomeComponent},
-  {path:'Home',component:HomeComponent},
-  {path:'Courses',component:CoursesComponent},
-  {path:'Contact',component:ContactComponent},
-  {path:'About',component:AboutComponent},
-  {path:'**',component:ErrorComponent}
-]
+
 
 @NgModule({
   declarations: [
@@ -26,11 +17,10 @@ const myRoutes:Routes=[
     AboutComponent,
     ContactComponent,
     CoursesComponent,
-    ErrorComponent
     
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(myRoutes)
+    BrowserModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
